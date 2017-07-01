e1cb35ff6c2f79fc64efe2d38f868a9a98f93367
github username <kevinladkins@gmail.com>
2017-07-01 16:12:31 -0400

    Build initialState

--- client/src/reducers/sourcesReducer.js
+++ client/src/reducers/sourcesReducer.js
@@ -1,4 +1,6 @@
-export default function sourcesReducer(state = [], action) {
+import initialState from './initialState'
+
+export default function sourcesReducer(state = initialState.sources, action) {
   switch (action.type) {
     case 'GET_SOURCES':
       return  action.payload
