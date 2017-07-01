b2446e00e615b22922368ba4ca3e7969dbdda11f
github username <kevinladkins@gmail.com>
2017-07-01 12:11:56 -0400

    Build Header component

---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () =>
+  <div className="App-header">
+    <h1>NEWSFEED</h1>
+  </div>
+
+
+
+
+export default Header
--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+
 import Home from '../components/Home'
+import Header from '../components/Header'
 import '../styles/App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h1>NEWSFEED</h1>
-        </div>
+          <Header />
           <Home />
       </div>
     );
