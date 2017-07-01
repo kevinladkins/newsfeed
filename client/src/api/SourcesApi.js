27eb2e8e9e308b629f742e627bc3362d00543b36
github username <kevinladkins@gmail.com>
2017-07-01 16:44:28 -0400

    Build shell for Newsfeed

--- client/src/api/SourcesApi.js
+++ client/src/api/SourcesApi.js
@@ -2,6 +2,6 @@ export function fetchSources() {
   return fetch('https://newsapi.org/v1/sources?language=en&country=us')
   .then(res => {
     return res.json()
-  }
+    }
   )
 }
---
+++ client/src/containers/Newsfeed.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux'
+
+
+class Newsfeed extends Component {
+
+  render() {
+    return(
+      <h1>Placeholder</h1>
+    )
+  }
+}
