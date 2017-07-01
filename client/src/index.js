45a78ac68c67ca740fa7382337e4e9942d7e99f0
github username <kevinladkins@gmail.com>
2017-07-01 14:20:07 -0400

    Connect Provider to Store

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,13 @@ import {Provider} from 'react-redux';
 
 import './styles/index.css';
 import App from './containers/App';
+import configureStore from './store/configureStore'
 import registerServiceWorker from './registerServiceWorker';
 
+const store = configureStore()
+
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
