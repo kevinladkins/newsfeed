02b37575433ed49a58839659b7d9511889e9e43e
github username <kevinladkins@gmail.com>
2017-07-01 14:41:33 -0400

    Build GetSources api and action

---
+++ client/src/actions/SourcesActions.js
@@ -0,0 +1,15 @@
+import {fetchSources} from '../SourcesApi'
+
+export function getSources() {
+  return function(dispatch) {
+    return fetchSources()
+      .then(response => {
+          dispatch({
+            type: 'GET_SOURCES'
+            payload: response.sources
+          })
+       }).catch(error => {
+        throw(error);
+       });
+     }
+  }
---
+++ client/src/api/SourcesApi.js
@@ -0,0 +1,6 @@
+export function fetchSources() {
+  return fetch('https://newsapi.org/v1/sources?language=en&country=us')
+  .then(res =>
+    return res.json()
+  )
+}
