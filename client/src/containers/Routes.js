b6e393ccaac3e6ea2d257952d7f27d4dc3f47616
github username <kevinladkins@gmail.com>
2017-07-01 13:13:48 -0400

    Add Switch to Routes

--- client/src/containers/Routes.js
+++ client/src/containers/Routes.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react'
-import {Route} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import Home from '../components/Home'
 
 
 export default class Routes extends Component {
   render() {
     return (
-      <div>
+      <Switch>
         <Route exact path='/' component={Home}/>
-      </div>
+      </Switch>
     )
   }
 }
