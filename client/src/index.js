2bb64fc37d0c382ae066ca71cafac1cb6b4b5717
github username <kevinladkins@gmail.com>
2017-07-01 12:15:59 -0400

    Add react-redux Provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+
 import './styles/index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
