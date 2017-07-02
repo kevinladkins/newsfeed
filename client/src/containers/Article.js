52c3d0843d7f6fe6a13b2b1a57f5f0b1d369793d
github username <kevinladkins@gmail.com>
2017-07-02 19:03:15 -0400

    Fix matchStateToProps

--- client/src/containers/Article.js
+++ client/src/containers/Article.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 
 
 const Article = (props) => {
+  console.log(props.article)
   return (
     <h1>Hiya</h1>
   )
@@ -10,7 +11,20 @@ const Article = (props) => {
 
 
 function mapStateToProps(state, ownProps) {
-  debugger
+  var articles = []
+    if (state.articles.find(channel => channel.name === ownProps.channelName)) {
+      articles = state.articles.find(channel => channel.name === ownProps.channelName).articles;
+    }
+  const article = articles.find(article => article.title === ownProps.match.params.article)
+    if (article) {
+      return {
+          article: article
+      }
+    } else {
+    return {
+      article: {}
+    }
+  }
 }
 
 export default connect(mapStateToProps)(Article)
