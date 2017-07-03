bdd0772f53307162630538a6ced0a5baccc82291
github username <kevinladkins@gmail.com>
2017-07-03 17:18:53 -0400

    Build checkbox click behaviors

--- client/src/containers/Source.js
+++ client/src/containers/Source.js
@@ -5,16 +5,40 @@ class Source extends Component {
   constructor(props) {
     super();
     this.state = {
-      selected: props.selected
+      selected: props.source.selected
+    }
+    this.clickCheckbox = this.clickCheckbox.bind(this)
+    this.setStyle = this.setStyle.bind(this)
+  }
+
+  clickCheckbox() {
+    this.state.selected === true ?
+      this.setState({
+        selected: false
+      })
+       :
+        this.setState({
+          selected: true
+        })
+
+    }
+
+  setStyle() {
+    if (this.state.selected === true) {
+      return {background: 'yellow'}
+    } else {
+      return {background: 'white'}
     }
   }
 
   render() {
+
+    var style = {background: 'yellow'}
     return(
-      <div className="source" >
+      <div className="source" onClick={this.clickCheckbox} style={this.setStyle()}>
         <h3>{this.props.source.name}</h3>
         <p>{this.props.source.description}</p>
-        <input type="checkbox" className="hidden" name={this.props.source.name} value={this.props.source.source_id} checked={this.state.selected} />
+        <input type="checkbox"  id={this.props.source.id} name={this.props.source.name} checked={this.state.selected} className="hidden" onChange={this.props.handleChange}/>
       </div>
     )
   }
