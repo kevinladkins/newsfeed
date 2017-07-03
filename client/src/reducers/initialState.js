f3dcacaaab81e6b9b2d38f3d7697ab596374fdc1
github username <kevinladkins@gmail.com>
2017-07-03 14:37:25 -0400

    Remove sort-by from initial channel state

--- client/src/reducers/initialState.js
+++ client/src/reducers/initialState.js
@@ -1,12 +1,10 @@
 const channels = [
   {
    source_id: "abc-news-au",
-   sort_by: "top",
    name:  "ABC News (AU)",
   },
   {
    source_id: "al-jazeera-english",
-   sort_by: "top",
    name: "Al Jazeera English",
   },
 ]
