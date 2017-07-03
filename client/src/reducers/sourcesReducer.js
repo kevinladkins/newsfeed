1e43d20a2d76ea2479d8c636d4dd707a0dc428d2
github username <kevinladkins@gmail.com>
2017-07-03 11:58:40 -0400

    Add #selected attribute to Sources

--- client/src/reducers/sourcesReducer.js
+++ client/src/reducers/sourcesReducer.js
@@ -3,7 +3,9 @@ import initialState from './initialState'
 export default function sourcesReducer(state = initialState.sources, action) {
   switch (action.type) {
     case 'GET_SOURCES':
-      return  action.payload
+      return action.payload.map (source => (
+        {...source, selected: false}
+      ))
     default:
       return state;
   }
