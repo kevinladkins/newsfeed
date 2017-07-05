9ab8a178cdfdce32e6501c102d9314d4b3b37705
github username <kevinladkins@gmail.com>
2017-07-05 13:25:13 -0400

    Add homepage boilerplate

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+
 const Home = () =>
-  <p className="App-intro">
-    Coming soon!
-  </p>
+    <p className="App-intro">
+      Welcome! Log in below to view your newsfeed.
+    </p>
+
 
 
 export default Home
