e1467636381295290384797f1ad95d2453e95f4b
github username <kevinladkins@gmail.com>
2017-07-05 16:28:09 -0400

    Build users#show

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   has_many :user_sources
   has_many :sources, through: :user_sources
+
+
+  def source_ids
+    self.sources.map {|source| source.name}
+  end
 end
