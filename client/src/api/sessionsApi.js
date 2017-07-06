d58ca8baec58dabce7e1ebf516a0276d634ac7b6
github username <kevinladkins@gmail.com>
2017-07-05 20:01:56 -0400

    Build login method

---
+++ client/src/api/sessionsApi.js
@@ -0,0 +1,13 @@
+
+export function login(userDetails) {
+    return fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({user: userDetails})
+    }).then(response =>
+      response.json()
+    )
+}
