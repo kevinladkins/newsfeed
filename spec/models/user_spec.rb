95af7248465bb37916a378071c62749d716cf3ad
github username <kevinladkins@gmail.com>
2017-07-06 13:42:40 -0400

    Passing email uniqueness validator

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,17 @@ RSpec.describe User, type: :model do
      end
 
 
-     it "requires email be unique"
+     it "requires email to be unique" do
+       user = User.new(password: 'password', email: 'kevinladkins@gmail.com')
+       user1 = User.new(password: 'passoword1', email: 'kevinladkins@gmail.com')
+       user.save
+       user1.save
+
+       expect(user.valid?).to be(true)
+       expect(user1.valid?).to be(false)
+       expect(user1.errors.full_messages).to eq(["Email has already been taken"])
+
+     end
 
 
      it "requires a password"
