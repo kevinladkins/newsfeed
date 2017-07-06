be961bf2baf41e1b2600750c170e6e0509b07a89
github username <kevinladkins@gmail.com>
2017-07-06 12:46:04 -0400

    Add signup link

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Login from '../containers/Login'
+import {Link} from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
       <p className="App-intro">
-        Welcome! Log in below to view your newsfeed.
+        Welcome! Log in below to view your newsfeed, or <Link to="/signup">click here</Link> to sign up.
       </p>
       <Login />
     </div>
