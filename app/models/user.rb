c932d43029c15ac0073c91997b2bb5444f3cf7f3
github username <kevinladkins@gmail.com>
2017-07-06 13:19:53 -0400

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   has_many :user_sources
   has_many :sources, through: :user_sources
 
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
 
   def source_ids
     self.sources.map {|source| source.name}
