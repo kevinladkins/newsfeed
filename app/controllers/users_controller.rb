77692701200349e69f040bf308a31de1af7f2fe1
github username <kevinladkins@gmail.com>
2017-07-06 13:19:27 -0400

    Add users#create route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
 
+  def create
+    
+
+  end
+
+
   def show
     user = User.find(params[:id])
     render json: {user: user, sources: user.source_ids}
