9bf3b9e548f0a585b905b8d0eb5ae7f0c4337590
github username <kevinladkins@gmail.com>
2017-07-05 20:02:37 -0400

    Build loginUser action

---
+++ client/src/actions/sessionsActions.js
@@ -0,0 +1,15 @@
+import {login} from '../api/sessionsApi'
+import {updateSources} from './sourcesActions'
+
+export function loginUser(userDetails) {
+  return function(dispatch) {
+    return login(userDetails).then(response => {
+        if (response.token) {
+          sessionStorage.setItem('jwt', response.token);
+          dispatch(updateSources(response.sources))
+        }
+      }).catch(error => {
+        throw(error);
+      });
+  }
+}
