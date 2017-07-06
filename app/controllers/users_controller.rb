946ba9240bcce3b58169e46c65c052983ee124e2
github username <kevinladkins@gmail.com>
2017-07-06 14:50:35 -0400

    Build users#create:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
 
   def create
-    
+    @user = User.new(user_params)
+    if @user.save
+      jwt = Auth.issue({user: @user.id})
+      render json: {user: {id: @user.id, email: @user.email}, sources: @user.source_ids, token: jwt}
+    else
+      render json: {errors: @user.errors},
+      status: 500
+    end
 
   end
 
@@ -9,5 +16,13 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     render json: {user: user, sources: user.source_ids}
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
