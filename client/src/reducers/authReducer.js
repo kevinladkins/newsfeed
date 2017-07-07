ffefd7edf4d495b7fee3a5f825cf36c979a2723c
github username <kevinladkins@gmail.com>
2017-07-06 20:08:41 -0400

    Build initial structure for authReducer

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,11 @@
+import initialState from './initialState'
+
+
+
+export default function authReducer(state = initialState.auth, action) {
+  switch(action.type) {
+
+    default:
+      return state
+  }
+}
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,12 +1,14 @@
 import {combineReducers} from 'redux';
 import sources from './sourcesReducer';
 import articles from './articlesReducer';
+import auth from '.authReducer';
 
 
 
 const rootReducer = combineReducers({
   sources,
-  articles
+  articles,
+  auth
 })
 
 export default rootReducer;
