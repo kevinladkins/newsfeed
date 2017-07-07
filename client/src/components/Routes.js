6ceb9b49ab9fa44ced90149fb52bbd5891dd0e3d
github username <kevinladkins@gmail.com>
2017-07-07 17:06:03 -0400

    Refactor routes as functional component

--- client/src/components/Routes.js
+++ client/src/components/Routes.js
@@ -1,21 +1,19 @@
 import React, {Component} from 'react'
 import {Route, Switch} from 'react-router-dom'
 
-import Home from '../components/Home'
-import Signup from './Signup'
-import Newsfeed from './Newsfeed'
-import Sources from './Sources'
+import Home from './Home'
+import Signup from '../containers/Signup'
+import Newsfeed from '../containers/Newsfeed'
+import Sources from '../containers/Sources'
 
 
-export default class Routes extends Component {
-  render() {
-    return (
+const Routes = () =>
       <Switch>
         <Route exact path='/' component={Home}/>
         <Route exact path='/sources' component={Sources} />
         <Route exact path='/signup' component={Signup} />
         <Route path='/newsfeed' component={Newsfeed} />
       </Switch>
-    )
-  }
-}
+
+
+export default Routes
--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router} from 'react-router-dom'
 
-import Routes from './Routes'
+import Routes from '../components/Routes'
 import Header from '../components/Header'
 import Navbar from '../components/Navbar'
 import '../styles/App.css';
