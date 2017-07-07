c9273f7ecbaffb4d57c06347089121f561c78134
github username <kevinladkins@gmail.com>
2017-07-07 12:24:59 -0400

    Add login actions

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -4,7 +4,14 @@ import initialState from './initialState'
 
 export default function authReducer(state = initialState.auth, action) {
   switch(action.type) {
-
+    case 'LOGGING_IN':
+      return Object.assign({}, state, {isAuthenticating: true})
+    case 'LOGGED_IN':
+      return {
+        isAuthenticating:false,
+        isAuthenticated: true,
+        user_id: action.payload
+      }
     default:
       return state
   }
