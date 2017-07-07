fc0a5beb0966f838356353d353de3fef51ca58b2
github username <kevinladkins@gmail.com>
2017-07-07 13:03:33 -0400

    Connect to state and actions, refactor conditional rendering and logout

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,47 +1,68 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {NavLink} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+
+import * as sessionsActions from '../actions/sessionsActions'
 import Auth from '../auth/authenticator'
 import '../styles/Navbar.css'
 
 
-function logout(event) {
-  event.preventDefault();
-  Auth.logOut();
-}
 
-const Navbar = () => {
-  if (Auth.loggedIn()) {
-    return (
-      <nav className="navbar">
-          <NavLink
-            className="navlink"
-            to="/newsfeed"
-          >Home</NavLink>
-          <NavLink
-            className="navlink"
-            to="/sources"
-          >Sources</NavLink>
-          <a href="#"
-            className="navlink"
-            onClick={(e) => {logout(e)}}
-          >Logout</a>
-      </nav>
-    )
-  } else {
-    return (
-      <nav className="navbar">
-          <NavLink
-            className="navlink"
-            to="/"
-          >Home</NavLink>
-          <NavLink
-            className="navlink"
-            to="/sources"
-          >Sources</NavLink>
-      </nav>
-    )
+
+class Navbar extends Component {
+  constructor(props) {
+    super();
+    this.logout = this.logout.bind(this)
   }
+
+  logout(event) {
+    event.preventDefault();
+    this.props.actions.logOutUser();
+  }
+
+  render() {
+    if (this.props.auth.isAuthenticated) {
+      return (
+        <nav className="navbar">
+            <NavLink
+              className="navlink"
+              to="/newsfeed"
+            >Home</NavLink>
+            <NavLink
+              className="navlink"
+              to="/sources"
+            >Sources</NavLink>
+            <a href="#"
+              className="navlink"
+              onClick={(e) => {this.logout(e)}}
+            >Logout</a>
+        </nav>
+      )
+    } else {
+      return (
+        <nav className="navbar">
+            <NavLink
+              className="navlink"
+              to="/"
+            >Home</NavLink>
+            <NavLink
+              className="navlink"
+              to="/sources"
+            >Sources</NavLink>
+        </nav>
+      )
+    }
+  }
+
+
 }
 
+const mapStateToProps = (state) => {
+  return {auth: state.auth}
+}
 
-export default Navbar
+const mapDispatchToProps = (dispatch) => {
+  return {actions: bindActionCreators(sessionsActions, dispatch)}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Navbar)
