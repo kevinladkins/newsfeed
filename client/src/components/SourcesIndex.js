1ab56524d13008b62cb7f504ea347c90ae7ae155
github username <kevinladkins@gmail.com>
2017-07-08 16:53:26 -0400

    Restyle page

--- client/src/components/SourcesIndex.js
+++ client/src/components/SourcesIndex.js
@@ -4,7 +4,7 @@ import BackButton from './BackButton'
 const SourcesIndex = ({saveSources, sourcesList}) =>
   <div className="sources-container">
     <div className="sources">
-      <h2>Select sources for your newsfeed:</h2>
+      <h2>Choose channels for your newsfeed (click to select):</h2>
       <button className="save-button" onClick={saveSources}>Save</button>
       <BackButton />
     </div>
