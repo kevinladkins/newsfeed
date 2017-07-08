9bd7065343bf25fcd3be1332018336f810b62801
github username <kevinladkins@gmail.com>
2017-07-08 15:57:48 -0400

    Move ChannelIndexView to components

---
+++ client/src/components/ChannelIndexView.js
@@ -0,0 +1,31 @@
+
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import Card from '../components/Card'
+
+
+
+function setArticleLink(title) {
+  return encodeURIComponent(title)
+}
+
+const ChannelIndexView = ({channel, articles}) => {
+    const articleObject = articles.find(articleObject => articleObject.name === channel.name);
+
+    const channelArticles = articleObject.articles.map((article, index) => {
+      return (
+        <Link to={`/newsfeed/${channel.source_id}/${setArticleLink(article.title)}`}><h4 className="article-title">{article.title}</h4></Link>
+      )
+    })
+
+    const title = (
+      <Link to={`/newsfeed/${channel.source_id}`}>{channel.name}</Link>
+    )
+
+    return (
+      <Card title={title} content={channelArticles}/>
+    )
+  }
+
+
+export default ChannelIndexView
