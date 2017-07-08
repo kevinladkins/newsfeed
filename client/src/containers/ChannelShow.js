679143dab98b8815f6f3d71cc923d0ba0d58aedd
github username <kevinladkins@gmail.com>
2017-07-08 14:17:53 -0400

    Refactor using app state instead of local state

--- client/src/containers/ChannelShow.js
+++ client/src/containers/ChannelShow.js
@@ -12,42 +12,24 @@ import * as channelsActions from '../actions/channelsActions'
 class ChannelShow extends Component {
   constructor(props) {
     super();
-    this.state = {
-      articles: []
-    }
-    this.setArticles = this.setArticles.bind(this)
+    this.handleClick = this.handleClick.bind(this)
     this.name = props.channel.name
   }
 
-  componentWillMount() {
-    this.setArticles()
-  }
-
-  componentDidMount() {
-    this.interval = setInterval(this.setArticles, 60000)
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval)
-  }
-
-  setArticles() {
-    this.props.actions.getArticles(this.props.channel).then(response => {
-      this.setState({
-        articles: response
-      })
-    })
-    this.handleClick = this.handleClick.bind(this)
-  }
 
   handleClick() {
     this.props.history.goBack()
   }
 
   render() {
-    const articles = this.state.articles.map((article, index) => (
-        <ArticleCard article={article} channel={this.props.channel} key={index} />
-    ))
+    var channelArticles = this.props.articles.find(articleObject => articleObject.name === this.props.channel.name);
+
+    if (!!channelArticles) {
+      channelArticles = channelArticles.articles.map((article, index) => (
+          <ArticleCard article={article} channel={this.props.channel} key={index} />
+      ))
+
+    }
 
     const ArticleWithProps = (props) => {
       return (
@@ -60,29 +42,22 @@ class ChannelShow extends Component {
   }
 
 
-
   return (
     <Switch>
       <Route exact path={this.props.match.url} render = {() =>
         <div>
           <h1>{this.name}</h1>
           <p onClick={this.handleClick}> &lt; &lt; Back</p>
-          {articles}
+          {channelArticles}
         </div>}/>
       <Route path={`${this.props.match.url}/:article`} render={ArticleWithProps} />
     </Switch>
    )}
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    actions: bindActionCreators(channelsActions, dispatch)
-  }
-}
 
 const mapStateToProps = (state, ownProps) => {
   const channel = state.sources.find(source => source.source_id == ownProps.match.params.ChannelId)
-
   if (channel) {
     return {
       articles: state.articles,
@@ -96,4 +71,4 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(ChannelShow)
+export default connect(mapStateToProps)(ChannelShow)
