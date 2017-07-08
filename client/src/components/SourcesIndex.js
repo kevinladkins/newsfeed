09d33c3635f80c26e63fb7abb6ece7a646514b0e
github username <kevinladkins@gmail.com>
2017-07-08 16:27:32 -0400

    Add BackButton

--- client/src/components/SourcesIndex.js
+++ client/src/components/SourcesIndex.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import BackButton from './BackButton'
 
 const SourcesIndex = ({saveSources, sourcesList}) =>
   <div className="sources-container">
     <div className="sources">
       <h2>Select sources for your newsfeed:</h2>
       <button className="save-button" onClick={saveSources}>Save</button>
+      <BackButton />
     </div>
     <div id="sources-form" >
       {sourcesList}
