54c624201590b3bc0be3ff93b9e0901f6cc32873
github username <kevinladkins@gmail.com>
2017-07-10 18:23:17 -0400

    Add message reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,13 +2,15 @@ import {combineReducers} from 'redux';
 import sources from './sourcesReducer';
 import articles from './articlesReducer';
 import auth from './authReducer';
+import message from './messageReducer'
 
 
 
 const rootReducer = combineReducers({
   sources,
   articles,
-  auth
+  auth,
+  message
 })
 
 export default rootReducer;
