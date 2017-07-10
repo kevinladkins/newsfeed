1fa643b8f74a6b0299c6e77d6fca15b7c7377ca6
github username <kevinladkins@gmail.com>
2017-07-10 18:23:47 -0400

    Remove message attribute

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -5,14 +5,15 @@ import initialState from './initialState'
 export default function authReducer(state = initialState.auth, action) {
   switch(action.type) {
     case 'LOGGING_IN':
-      return Object.assign({}, state, {isAuthenticating: true, message: "Logging in..."})
+      return Object.assign({}, state, {isAuthenticating: true})
     case 'LOGGED_IN':
       return {
         isAuthenticating:false,
         isAuthenticated: true,
         user_id: action.payload,
-        message: ''
       }
+     case 'LOGIN_ERROR':
+       return initialState.auth;
       case 'LOG_OUT':
         return initialState.auth;
     default:
