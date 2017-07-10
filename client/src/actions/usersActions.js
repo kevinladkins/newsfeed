fb2ad58e0fe409ba768c6c07cbcf86da419cc5f3
github username <kevinladkins@gmail.com>
2017-07-10 18:02:49 -0400

    Refactor with functions

--- client/src/actions/usersActions.js
+++ client/src/actions/usersActions.js
@@ -1,18 +1,27 @@
 import {postNewUser} from '../api/usersApi'
+import {loggedIn} from './sessionsActions'
+
+export const creatingUser = () => {
+  return {type: 'CREATING_USER'}
+}
+
+export const signupError = (response) => {
+  return {
+    type: 'SIGNUP_ERROR',
+    payload: response.errors
+  }
+}
 
 export function createUser(userDetails, history) {
   return function(dispatch) {
-    dispatch({type: 'LOGGING_IN'})
+    dispatch(creatingUser())
     return postNewUser(userDetails).then(response => {
         if (response.token) {
           sessionStorage.setItem('jwt', response.token);
-          dispatch(
-            {
-              type: 'LOGGED_IN',
-              payload: response.user.id
-            }
-          );
+          dispatch(loggedIn(response));
           history.push('/newsfeed')
+        } else {
+          dispatch(signupError(response));
         }
       }).catch(error => {
         throw(error);
