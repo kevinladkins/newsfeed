a40f8e8464462e96cf78314098122bef99b2479f
github username <kevinladkins@gmail.com>
2017-07-11 13:06:05 -0400

    Move setUrl functoin to parent

--- client/src/components/ArticleCard.js
+++ client/src/components/ArticleCard.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-function setArticleUrl(title) {
-  var sanitizedTitle = title.replace(/%/g, "[percent]")
-  debugger
-  return encodeURIComponent(sanitizedTitle)
-}
 
-const ArticleCard = ({channel, article}) => {
+
+const ArticleCard = ({channel, article, setArticleUrl}) => {
   return (
     <Link to={`/newsfeed/${channel.source_id}/${setArticleUrl(article.title)}`} className="article-link">
       <div className="card">
