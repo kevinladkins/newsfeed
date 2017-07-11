3e7ac17cb420073af6ef21523e448fd7b28178c6
github username <kevinladkins@gmail.com>
2017-07-11 13:31:28 -0400

    Move setArticleUrl to parent

--- client/src/components/ArticlesIndex.js
+++ client/src/components/ArticlesIndex.js
@@ -2,9 +2,10 @@ import React from 'react'
 import ArticleCard from './ArticleCard'
 import BackButton from './BackButton'
 
-const ArticlesIndex = ({articles, channel}) => {
+const ArticlesIndex = ({articles, channel, setArticleUrl}) => {
+
     const articleList = articles.map((article, index) => {
-      return <ArticleCard article={article} channel={channel} key={index} />
+      return <ArticleCard article={article} channel={channel} setArticleUrl={setArticleUrl} key={index} />
     })
 
     return (
